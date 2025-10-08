ea747e8409dc2cde08776e788e7cadca6460c454
JUAN MONTOYA <judamo18@gmail.com>
2025-10-08 00:36:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -58,17 +58,17 @@ if submitted:
     #st.error(input_data)
     # --- Preprocessing Input Data ---
     # Apply label encoding
-    """try:
-        input_data['Sex_encoded'] = le.transform(input_data['Sex'])
-        input_data['Housing_encoded'] = le.transform( input_data[['Housing']] )
-        input_data['Saving accounts_encoded'] = le.transform(input_data[['Saving accounts']])
+    try:
+        input_data['Sex_encoded'] = le.fit_transform(input_data['Sex'])
+        input_data['Housing_encoded'] = le.fit_transform( input_data[['Housing']] )
+        input_data['Saving accounts_encoded'] = le.fit_transform(input_data[['Saving accounts']])
         
     except ValueError as e:
         st.error(f"valor1:{housing}valor2: {input_data[['Housing']]} ")
         st.error(f"valor1:{housing}valor2: {input_data[['Saving accounts']]} ")
         st.error(f"valor3:{input_data['Housing'][0]} ")
         st.error(f"Error during label encoding input: {e}. Please check the input values.")
-        st.stop()"""
+        st.stop()
 
     # Apply one-hot encoding to 'Job'
     try:
